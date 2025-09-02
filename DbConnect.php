bb78107ea528bd6e1ca603945d80fa02c22e827b
NikaDolezal <nika.dolezal@gmail.com>
2025-09-02 12:42:21 +0200

    Update DbConnect.php

--- DbConnect.php
+++ DbConnect.php
@@ -5,6 +5,7 @@ class DbConnect
     public $server = 'db26494.public.databaseasp.net';
     public $dbname = 'db26494';
     public $user = 'db26494';
-    public $pass = 'Q!q7?9WwMx8@';    
+    public $pass = '';    
 }
+
 ?>
