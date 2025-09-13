b21605ec1e1eea34e551bdf6959cd9cefa0ec55f
NikaDolezal <nika.dolezal@gmail.com>
2025-09-13 20:53:31 +0200

    added genre and location, tidied up

--- ab_parse.php
+++ ab_parse.php
@@ -7,37 +7,46 @@
 </head>
 <body>
     <?php
-    echo "Hello world! <br>";
-    $file = fopen("audiobooks_list.txt", "r");
+	echo "Hello world! <br>";
+	//open file
+   	$file = fopen("audiobooks_list.txt", "r");
 
-//parse lines until EOF
-while(! feof($file)) {
-  $line = fgets($file);
-  if ( str_starts_with( $line, "Location" ) ){
-    // sscanf($line, "");
-  }
-  
-  if ( !( str_starts_with( $line, "* " ) ) ){
-    //'* ' is only present in lines that contain an entry, rest is printed as-is
-    echo $line."<br>";
-    } else {
-    //testing sscanf()
-    $auth_name = '';
-    $auth_surname = '';
-    $title = '';
-    $notes = '';
-    $pattern = "* %[^,], %[^:]: %[^>]> %[0-9A-Za-z/ -]+";
-    sscanf($line, $pattern, $auth_surname, $auth_name, $title, $notes);
-    if($notes === ''){
-    echo "author surname: ".$auth_surname." author name: ".$auth_name.", book title: ".trim($title).". Notes: ".$notes."<br />";
-    } else {
-    echo "author surname: ".$auth_surname." author name: ".$auth_name.", book title: ".trim($title, " -").". Notes: ".$notes."<br />";
+	//initialize block vars
+	$location = '';
+	$genre = '';
 
-    }
-  }
-}
+	//set patterns
+	$block_pattern = "%s %[0-9A-Za-z/ -]+";
+	$line_pattern = "* %[^,], %[^:]: %[^>]> %[0-9A-Za-z/ -]+";
 
-fclose($file);
+	//parse lines until EOF
+	while(! feof($file)) {
+		$line = fgets($file);
+
+		//resolve block values - each sits on its own line, once a block value is recognized and saved, the cycle can move on, format does not allow for a book entry to still follow
+		if ( str_contains( $line, "Location:" ) ){
+			sscanf($line, $block_pattern, $tmp, $location);
+			continue;
+	 	}
+	  	if ( str_contains( $line, "Genre:" ) ){
+			sscanf($line, $block_pattern, $tmp, $genre);
+			continue;
+	  	}
+
+		//parse a book entry line - lines that fail to begin with '*' will be ignored even if otherwise correctly formatted
+		if ( str_starts_with( $line, "*" ) ){
+			//initialize line vars, values must not carry over to next line in case of an empty space/read err
+    			$auth_name = '';
+    			$auth_surname = '';
+    			$title = '';
+    			$notes = '';
+    			//TODO multiple authors
+    			sscanf($line, $line_pattern, $auth_surname, $auth_name, $title, $notes);
+    			echo "author surname: ".$auth_surname.", author name: ".$auth_name.", book title: ".trim($title).", genre: ".$genre.", location: ".$location.". Notes: ".$notes."<br />";
+  		}
+	}
+
+	fclose($file);
     ?>
 </body>
 </html>
